d1c3cc41e4c868d4c2dd015053b539cfc8dd4455
akong <noobakong@163.com>
2023-08-24 02:34:42 +0800

    chore: @vitejs/plugin-react-swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
